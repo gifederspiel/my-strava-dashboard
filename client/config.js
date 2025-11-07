bade4fa713a221d6a8fb790ca0bf90b02ab6e71d
Gian Federspiel <gian.federspiel@bluewin.ch>
2025-11-07 10:18:29 +0100

    changed API URL

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   window.__STRAVA_API_BASE_URL__ ||
   import.meta?.env?.VITE_STRAVA_API_BASE_URL ||
-  'http://localhost:4000';
+  'https://my-strava-dashboard.onrender.com';
 
 export const LATEST_ACTIVITY_COUNT = 10;
 
